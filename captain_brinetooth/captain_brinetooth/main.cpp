8b5ad79dc20bfeb90d1ba8a1fb4c05b313b51e96
Jose Daniel <jrave@ucm.es>
2021-03-03 19:45:18 +0100

    Arreglado el fallo de librerías Box2D y SDL

--- captain_brinetooth/captain_brinetooth/main.cpp
+++ captain_brinetooth/captain_brinetooth/main.cpp
@@ -1,7 +1,16 @@
+#include <iostream>;
 #include "SDL.h"
 #include "box2d.h"
 
+using namespace std;
+
+void CrearJuego()
+{
+	cout << "maricon";
+}
+
 int main(int argc, char* argv[])
 {
+	CrearJuego();
 	return 0;
 }
