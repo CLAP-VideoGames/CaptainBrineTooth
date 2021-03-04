71f878a9a6fb2653ab3394a8efc6e92423578aa9
adriac02 <adriac02@ucm.es>
2021-03-04 04:22:54 +0100

    Añadida una canción funcionando

--- captain_brinetooth/captain_brinetooth/main.cpp
+++ captain_brinetooth/captain_brinetooth/main.cpp
@@ -7,7 +7,26 @@ using namespace std;
 
 void CrearJuego()
 {
-	cout << "maricon";
+	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
+	
+	Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
+	Mix_Music* song;
+	song = Mix_LoadMUS("../../../../Assets/music/song1.mp3");
+
+	//Creacion de ventana provisional
+	SDL_Window* window = SDL_CreateWindow("Captain Brinetooth", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_OPENGL);
+
+	Mix_PlayMusic(song, 0);
+
+	//Bucle para probar que funciona la música
+	bool run = true;
+	SDL_Event event;
+	while (SDL_PollEvent(&event) || run) {
+		if (event.type == SDL_KEYDOWN) {
+			run = false;
+			SDL_QUIT;
+		}
+	}
 }
 
 int main(int argc, char* argv[])
