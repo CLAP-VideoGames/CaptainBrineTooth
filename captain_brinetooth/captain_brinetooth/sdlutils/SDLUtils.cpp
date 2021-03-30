f5ced24e8281f8942a78b5c8b65eb2c376165a44
Angelo06 <angelo06@ucm.es>
2021-03-30 17:29:38 +0200

    El proyecto ya no deja basura

--- captain_brinetooth/captain_brinetooth/sdlutils/SDLUtils.cpp
+++ captain_brinetooth/captain_brinetooth/sdlutils/SDLUtils.cpp
@@ -87,22 +87,22 @@ void SDLUtils::loadReasources(std::string filename) {
 	// is correct.
 
 	// load JSON configuration file
-	JSONValue *jValue = JSON::ParseFromFile(filename);
+	JSONValue *jaux = JSON::ParseFromFile(filename);
 
 	// check it was loaded correctly
 	// the root must be a JSON object
-	if (jValue == nullptr || !jValue->IsObject()) {
+	if (jaux == nullptr || !jaux->IsObject()) {
 		throw "Something went wrong while load/parsing '" + filename + "'";
 	}
 
 	// we know the root is JSONObject
-	JSONObject root = jValue->AsObject();
+	JSONObject root = jaux->AsObject();
 
 	// TODO improve syntax error checks below, now we do not check
 	//      validity of keys with values as sting or integer
 
 	// load fonts
-	jValue = root["fonts"];
+	JSONValue* jValue = root["fonts"];
 	if (jValue != nullptr) {
 		if (jValue->IsArray()) {
 			for (auto &v : jValue->AsArray()) {
@@ -173,6 +173,7 @@ void SDLUtils::loadReasources(std::string filename) {
 		} else {
 			throw "'messages' is not an array in '" + filename + "'";
 		}
+		
 	}
 
 	// load sounds
@@ -236,6 +237,7 @@ void SDLUtils::loadReasources(std::string filename) {
 			throw "'tilesets' is not an array in '" + filename + "'";
 		}
 	}
+	delete jaux;
 }
 
 void SDLUtils::closeSDLExtensions() {
