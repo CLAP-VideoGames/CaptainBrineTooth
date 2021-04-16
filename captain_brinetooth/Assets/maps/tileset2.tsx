76f14ffc299b9ad58c1b83e05bd4d4dd09bde861
Legos <62616911+omaya01@users.noreply.github.com>
2021-04-16 21:38:16 +0200

    Primera tanda de tiles
    Con estos los números se quedan en 10 principios, 10 intermedias y 5 finales para el primer nivel (intentaré que todos tengan estos número para final de iteración)

---
+++ captain_brinetooth/Assets/maps/tileset2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="tileset2" tilewidth="64" tileheight="64" tilecount="36" columns="9">
+ <image source="../tileset/tileset2.png" width="576" height="256"/>
+</tileset>
