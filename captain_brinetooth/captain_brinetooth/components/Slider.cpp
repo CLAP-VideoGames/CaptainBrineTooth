5d5187d906416c71ef6203c6b6c997d612f54a8e
Jose Daniel <jrave@ucm.es>
2021-04-16 15:23:49 +0200

    Arreglo de fallo de compilación

--- captain_brinetooth/captain_brinetooth/components/Slider.cpp
+++ captain_brinetooth/captain_brinetooth/components/Slider.cpp
@@ -1,6 +1,6 @@
 #include "Slider.h"
 
-Slider::Slider(const Vector2D& pos_, const std::pair<Vector2D, Vector2D>& sizes_, Texture* texts[NUM_TEXTURES], void(*callback)(float&)){
+Slider::Slider(const Vector2D& pos_, const std::pair<Vector2D, Vector2D>& sizes_, Texture* texts[NUM_TEXTURES], void(*callback)(float)){
 	pos = pos_;
 	sizes = sizes_;
 	for (int i = 0; i < NUM_TEXTURES; i++) textures[i] = texts[i];
