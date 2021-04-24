47835749ea3802f0cb572bd57dcd35e3a37e286d
Jose Daniel <jrave@ucm.es>
2021-04-24 21:33:48 +0200

    Añadida librería FFMPEG

--- captain_brinetooth/captain_brinetooth/components/VideoPlayer.cpp
+++ captain_brinetooth/captain_brinetooth/components/VideoPlayer.cpp
@@ -1,14 +1,14 @@
 #include "VideoPlayer.h"
 
-VideoPlayer::VideoPlayer(const std::string file){
-	files.push_back(file);
+VideoPlayer::VideoPlayer(const std::string file, bool loop){
+	files.push_back({file, loop});
 }
 
 VideoPlayer::~VideoPlayer(){
 }
 
-void VideoPlayer::queueVideo(const std::string file){
-	files.push_back(file);
+void VideoPlayer::queueVideo(const std::string file, bool loop){
+	files.push_back({ file, loop });
 }
 
 void VideoPlayer::init()
--- captain_brinetooth/captain_brinetooth/components/VideoPlayer.h
+++ captain_brinetooth/captain_brinetooth/components/VideoPlayer.h
@@ -3,18 +3,46 @@
 #include "../ecs/Component.h"
 #include <string>
 #include <vector>
+#include "SDL.h"
+
+//Librerias de ffmpeg
+	#include <iostream>
+	#include <libavcodec/avcodec.h>
+	#include <libavformat/avformat.h>
+	#include <libswscale/swscale.h>
+	#include <libavutil/imgutils.h>
+
 class VideoPlayer : public Component
 {
 public:
-	VideoPlayer(const std::string file);
+	VideoPlayer(const std::string file, bool loop);
 	~VideoPlayer();
 
-	void queueVideo(const std::string file);
 	void init() override;
+	
 	void update() override;
 	void render() override;
 
+	void queueVideo(const std::string file, bool loop);
+
 private:
-	std::vector<std::string> files;
+	std::vector<std::pair<std::string, bool>> files;
+
+	AVFormatContext* pFormatCtx;
+	AVCodecContext* pCodecCtx;
+	AVCodec* pCodec;
+	AVFrame* pFrame, * pFrameYUV;
+	unsigned char* out_buffer;
+	AVPacket* packet;
+	struct SwsContext* img_convert_ctx;
+
+	// sdl stuff
+	//
+	int window_w, window_h;
+	SDL_Window* sdlWindow;
+	SDL_Renderer* sdlRenderer;
+	SDL_Texture* sdlTexture;
+	SDL_Rect sdlRect;
+	SDL_Event event;
 };
 
