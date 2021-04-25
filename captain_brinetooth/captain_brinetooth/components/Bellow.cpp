97bfd4369f1a1187d5884956610b9310ead768d4
mihern07 <mihern07@ucm.es>
2021-04-25 21:21:58 +0200

    Añadido esquema escupetintas

---
+++ captain_brinetooth/captain_brinetooth/components/Bellow.cpp
@@ -0,0 +1,128 @@
+#include "Bellow.h"
+
+#include <iostream>
+#include "../game/CollisionLayers.h"
+#include "AnimBlendGraph.h"
+
+using namespace ColLayers;
+
+void Bellow::init() {
+	tr_ = entity_->getComponent<Transform>();
+	assert(tr_ != nullptr);
+	anim_ = entity_->getComponent<AnimBlendGraph>();
+
+	bulletVelocity = 100;
+}
+
+void Bellow::update() {
+	if (ih().keyDownEvent()) {
+		if (ih().isKeyDown(SDL_SCANCODE_E)) {
+
+			//Player not attacking or in combo
+			if (CURRENT_STATUS == STATUS::Iddle && overheat <= maxOverheat && startedReloading + reloadingTime < sdlutils().currRealTime()) {
+				std::cout << "Started shooting\n";
+
+				//Set player as sawing
+				CURRENT_STATUS = STATUS::Shooting;
+
+				//Activate attack animation + sawing on attack
+				if (anim_->getParamIndex("machineGun_att") != -1)
+					anim_->setParamValue("machineGun_att", 1);
+
+				//Shoot
+				shoot();
+
+				//Time control variables
+				shotActivationTime = sdlutils().currRealTime();
+			}
+		}
+	}
+
+	if (ih().keyUpEvent()) {
+		if (ih().isKeyUp(SDL_SCANCODE_E) && CURRENT_STATUS == STATUS::Shooting) {
+			std::cout << "Stopped shooting\n";
+
+			CURRENT_STATUS = STATUS::Iddle;
+
+			//Desactivar animacion
+			if (anim_->getParamIndex("machineGun_att") != -1)
+				anim_->setParamValue("machineGun_att", 0);
+		}
+	}
+
+	//Check out of input cases
+	if (CURRENT_STATUS == STATUS::Shooting && overheat >= maxOverheat) {
+		//Deactivate chainsaw
+		std::cout << "Started reload\n";
+		CURRENT_STATUS = STATUS::Reloading;
+
+		startedReloading = sdlutils().currRealTime();
+		entity_->getMngr()->getSoundMngr()->playSoundEffect("recarga_anchoa", 0);
+
+
+		if (anim_->getParamIndex("machineGun_att") != -1)
+			anim_->setParamValue("machineGun_att", 2);
+	}
+	else if (CURRENT_STATUS == STATUS::Shooting && overheatSpikeTime + timeBetweenOverheatSpikes < sdlutils().currRealTime()) {
+
+		overheat++;
+
+		std::cout << overheat << "\n";
+
+		overheatSpikeTime = sdlutils().currRealTime();
+	}
+	else if (CURRENT_STATUS == STATUS::Reloading && startedReloading + reloadingTime < sdlutils().currRealTime()) {
+		//Deactivate animation lock
+		std::cout << "Stopped reloading\n";
+		CURRENT_STATUS = STATUS::Iddle;
+
+		if (!ih().isKeyDown(SDL_SCANCODE_E)) {
+			if (anim_->getParamIndex("machineGun_att") != -1)
+				anim_->setParamValue("machineGun_att", 0);
+		}
+
+		overheat = 0;
+	}
+
+	//Decrementamos el overheat
+	if (CURRENT_STATUS == STATUS::Iddle && overheat > 0 && decreaseOverheatSpikeTime + timeBetweenOverheatSpikes < sdlutils().currRealTime()) {
+		overheat--;
+
+		std::cout << overheat << "\n";
+		decreaseOverheatSpikeTime = sdlutils().currRealTime();
+	}
+
+	//Comprobamos si hay que disparar una bala
+	if (CURRENT_STATUS == STATUS::Shooting && shotActivationTime + timeBetweenShots < sdlutils().currRealTime()) {
+
+		//Shoot
+		shoot();
+
+		shotActivationTime = sdlutils().currRealTime();
+	}
+}
+
+void Bellow::shoot() {
+	Entity* bullet = entity_->getMngr()->addEntity();
+
+	Vector2D bulletpos; Vector2D bulletvel;
+	if (anim_->isFlipX()) {
+		bulletpos = tr_->getPos() + Vector2D(+60, 0);
+		bulletvel = Vector2D(1, 0);
+	}
+	else {
+		bulletpos = tr_->getPos() + Vector2D(-60, 0);
+		bulletvel = Vector2D(-1, 0);
+	}
+
+	entity_->getMngr()->getSoundMngr()->playSoundEffect("disparo_anchoa", 700);
+
+	bullet->addComponent<Transform>(bulletpos, Vector2D(0, 0), 100.0f, 30.0f, 0.0f);
+	AnimBlendGraph* anim_controller = bullet->addComponent<AnimBlendGraph>();
+	anim_controller->addAnimation("iddle", &sdlutils().images().at("machine_gun_bullet"), 1, 1, 1, 1, 1);
+	bullet->addComponent<DisableOnExit>();
+	bullet->addComponent<BoxCollider>(DYNAMIC, PLAYER_ATTACK, PLAYER_ATTACK_MASK);
+	bullet->getComponent<BoxCollider>()->applyForce(bulletvel, bulletVelocity);
+	bullet->addComponent<WeaponDamageDetection>(20);
+	bullet->addComponent<DestroyOnCollision>(); // esto da problemas si no se hacen bien las capas de colision?
+}
---
+++ captain_brinetooth/captain_brinetooth/components/Bellow.h
@@ -0,0 +1,57 @@
+#pragma once
+
+
+#include <SDL.h>
+#include <cassert>
+#include "Transform.h"
+#include "BoxCollider.h"
+#include "../ecs/Entity.h"
+#include "../ecs/Component.h"
+#include "../sdlutils/InputHandler.h"
+#include "../sdlutils/SDLUtils.h"
+#include "../ecs/Manager.h"
+#include "AnimBlendGraph.h"
+#include "WeaponDamageDetection.h"
+#include "PlayerController.h"
+#include "DisableOnExit.h"
+#include "DestroyOnCollision.h"
+
+
+class Bellow : public Component {
+public:
+	Bellow() :
+		tr_(nullptr) {
+	}
+	virtual ~Bellow() {
+		if (anim_->getParamIndex("chainsaw_att") != -1)
+			anim_->setParamValue("chainsaw_att", 0);
+	}
+
+	void init() override;
+
+	void update() override;
+
+	void shoot();
+private:
+	enum STATUS { Iddle, Shooting, Reloading };
+
+	Transform* tr_;
+	AnimBlendGraph* anim_controller;
+
+	//Local control variables (don't touch or change them except in code)
+	int CURRENT_STATUS = STATUS::Iddle;
+	float startedReloading;
+	float shotActivationTime;
+	bool currentlyShooting = false;
+	float overheat;
+	float overheatSpikeTime;
+	float decreaseOverheatSpikeTime;
+	float bulletVelocity;
+	AnimBlendGraph* anim_;
+
+	//Cooldown variables (should be able to be changed as you see fit)
+	float reloadingTime = 400;		//Counted after one attack is stopped to avoid animation clipping
+	float timeBetweenShots = 100;
+	float timeBetweenOverheatSpikes = 100;
+	float maxOverheat = 30;
+};
