8e346afba965a42c4bf22a19cf923da47f9057f4
Jose Daniel <jrave@ucm.es>
2021-04-28 00:15:06 +0200

    Arreglado fallo de compilacion

--- captain_brinetooth/captain_brinetooth/game/App.cpp
+++ captain_brinetooth/captain_brinetooth/game/App.cpp
@@ -31,9 +31,9 @@ App::App() {
 
 	SoundManager* sndProvisional = new SoundManager(0, "Menu");
 	//----Inicio de Intro----
-	//stateMachine->pushState(new IntroState(this, world_, sndProvisional));
+	stateMachine->pushState(new IntroState(this, world_, sndProvisional));
 	//----Inicio por defecto----
-	stateMachine->pushState(new MenuState(this, world_, sndProvisional));
+	//stateMachine->pushState(new MenuState(this, world_, sndProvisional));
 	//-----Zona de pruebas------
 	//stateMachine->pushState(new TestZoneState(this, world_, sndProvisional));
 	//stateMachine->pushState(new PescaState(this, world_, sndProvisional));
--- captain_brinetooth/captain_brinetooth/states/IntroState.cpp
+++ captain_brinetooth/captain_brinetooth/states/IntroState.cpp
@@ -8,20 +8,25 @@ IntroState::IntroState(App* a, std::shared_ptr<b2World> mundo, SoundManager* snd
 IntroState::~IntroState(){
 }
 
-void IntroState::init(){
+void IntroState::init() {
 	video = manager_->addEntity();
 
 	std::vector<std::pair<const char*, std::pair<bool, int>>> videos;
 	//Filename, loop, frameRate
-	std::pair<const char*, std::pair<bool, int>> video_ = { "assets/videos/0001-0120.mp4", {true,38} };
+	std::pair<const char*, std::pair<bool, int>> video__ = { "assets/videos/intro_loop.mp4", {true, 1} };
+	videos.push_back(video__);
+	std::pair<const char*, std::pair<bool, int>> video_ = { "assets/videos/intro.mp4", {false,1} };
 	videos.push_back(video_);
-	//std::pair<const char*, std::pair<bool, int>> video__ = { "assets/videos/water.mp4", {false, 38}};
-	//videos.push_back(video__);
 
-	VideoPlayer* compVideo = video->addComponent<VideoPlayer>(videos);
+	//VideoPlayer* compVideo = video->addComponent<VideoPlayer>(videos);
 	manager_->getSoundMngr()->setGeneralVolume(40);
 	manager_->getSoundMngr()->playIntroMusic();
-	SDL_Rect videoRect = compVideo->getRect();
+	//SDL_Rect videoRect = compVideo->getRect();
+	SDL_Rect videoRect;
+	videoRect.x = 0;
+	videoRect.y = 0;
+	videoRect.w = (int)(App::camera.w * App::camera_Zoom_Out);
+	videoRect.h = (int)(App::camera.h * App::camera_Zoom_Out);
 
 	fade = manager_->addEntity();
 	fadeComp = fade->addComponent<Fade>(Vector2D(videoRect.w, videoRect.h), Vector2D(videoRect.x, videoRect.y), 3000, 3000);
