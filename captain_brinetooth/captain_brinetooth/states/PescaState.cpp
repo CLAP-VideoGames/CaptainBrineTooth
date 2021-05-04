050e52381c2d4277c8dd44be8a099b5fabd9a9f3
Daniel <druiz07@ucm.es>
2021-05-04 14:52:03 +0200

    Añadidos comentarios en la creación de recompensas para Adriá

--- captain_brinetooth/captain_brinetooth/states/PescaState.cpp
+++ captain_brinetooth/captain_brinetooth/states/PescaState.cpp
@@ -119,33 +119,36 @@ void PescaState::createRandomReward(const Config& entityConfig)
 			if (rnd >= 0 && rnd < 5)
 			{
 				reward0_anim_controller->addAnimation("idle", &sdlutils().images().at("piedra"), 1, 1, 1, 1, 0);
-
+				reward0->addComponent<BoxCollider>(entityConfig.physicType, entityConfig.col, entityConfig.colMask);
 
 			}
 			else if (rnd >= 6 && rnd < 10)
 			{
 
 				reward0_anim_controller->addAnimation("idle", &sdlutils().images().at("casco"), 1, 1, 1, 1, 0);
+				reward0->addComponent<BoxCollider>(entityConfig.physicType, entityConfig.col, entityConfig.colMask);
 
 			}
 			else  if (rnd >= 10 && rnd <= 14)
 			{
 
 				reward0_anim_controller->addAnimation("idle", &sdlutils().images().at("lata"), 1, 1, 1, 1, 0);
+				//Pones a pelo tanto el tipo como el colision mask-> acuerdate de poner la capa para que solo toque el gancho , es decir que es para cuando el gancho baja 
+				reward0->addComponent<BoxCollider>(entityConfig.physicType, entityConfig.col, entityConfig.colMask);
 
 			}
 			random = 30;
 		}
-		reward0->addComponent<BoxCollider>(entityConfig.physicType, entityConfig.col, entityConfig.colMask);
+
 		reward0->addComponent<Reward>(random);
 
 
 	}
 	for (int i = 0; i < entitiesPerLine; i++)
 	{
-		random = sdlutils().rand().teCuoto(0, 3);
+		random = sdlutils().rand().teCuoto(0, 7);
 
-		auto* reward0 = createBasicEntity(Vector2D(pos2.getX() + (200 * i) , pos2.getY() ), Vector2D(70, 70), 0.0f, Vector2D(0, 0));
+		auto* reward0 = createBasicEntity(Vector2D(pos1.getX() + (200 * i), pos1.getY()), Vector2D(70, 70), 0.0f, Vector2D(0, 0));
 		AnimBlendGraph* reward0_anim_controller = reward0->addComponent<AnimBlendGraph>();
 		if (random == 0)
 		{
@@ -167,32 +170,36 @@ void PescaState::createRandomReward(const Config& entityConfig)
 			if (rnd >= 0 && rnd < 5)
 			{
 				reward0_anim_controller->addAnimation("idle", &sdlutils().images().at("piedra"), 1, 1, 1, 1, 0);
-
+				reward0->addComponent<BoxCollider>(entityConfig.physicType, entityConfig.col, entityConfig.colMask);
 
 			}
 			else if (rnd >= 6 && rnd < 10)
 			{
 
 				reward0_anim_controller->addAnimation("idle", &sdlutils().images().at("casco"), 1, 1, 1, 1, 0);
+				reward0->addComponent<BoxCollider>(entityConfig.physicType, entityConfig.col, entityConfig.colMask);
 
 			}
 			else  if (rnd >= 10 && rnd <= 14)
 			{
 
 				reward0_anim_controller->addAnimation("idle", &sdlutils().images().at("lata"), 1, 1, 1, 1, 0);
+				//Pones a pelo tanto el tipo como el colision mask-> acuerdate de poner la capa para que solo toque el gancho , es decir que es para cuando el gancho baja 
+				reward0->addComponent<BoxCollider>(entityConfig.physicType, entityConfig.col, entityConfig.colMask);
 
 			}
 			random = 30;
 		}
-		reward0->addComponent<BoxCollider>(entityConfig.physicType, entityConfig.col, entityConfig.colMask);
+
 		reward0->addComponent<Reward>(random);
+
 
 	}
 	for (int i = 0; i < entitiesPerLine; i++)
 	{
-		random = sdlutils().rand().teCuoto(0, 3);
+		random = sdlutils().rand().teCuoto(0, 7);
 
-		auto* reward0 = createBasicEntity(Vector2D(pos3.getX() + (200 * i), pos3.getY()), Vector2D(70, 70), 0.0f, Vector2D(0, 0));
+		auto* reward0 = createBasicEntity(Vector2D(pos1.getX() + (200 * i), pos1.getY()), Vector2D(70, 70), 0.0f, Vector2D(0, 0));
 		AnimBlendGraph* reward0_anim_controller = reward0->addComponent<AnimBlendGraph>();
 		if (random == 0)
 		{
@@ -214,25 +221,29 @@ void PescaState::createRandomReward(const Config& entityConfig)
 			if (rnd >= 0 && rnd < 5)
 			{
 				reward0_anim_controller->addAnimation("idle", &sdlutils().images().at("piedra"), 1, 1, 1, 1, 0);
-
+				reward0->addComponent<BoxCollider>(entityConfig.physicType, entityConfig.col, entityConfig.colMask);
 
 			}
 			else if (rnd >= 6 && rnd < 10)
 			{
 
 				reward0_anim_controller->addAnimation("idle", &sdlutils().images().at("casco"), 1, 1, 1, 1, 0);
+				reward0->addComponent<BoxCollider>(entityConfig.physicType, entityConfig.col, entityConfig.colMask);
 
 			}
 			else  if (rnd >= 10 && rnd <= 14)
 			{
 
 				reward0_anim_controller->addAnimation("idle", &sdlutils().images().at("lata"), 1, 1, 1, 1, 0);
+				//Pones a pelo tanto el tipo como el colision mask-> acuerdate de poner la capa para que solo toque el gancho , es decir que es para cuando el gancho baja 
+				reward0->addComponent<BoxCollider>(entityConfig.physicType, entityConfig.col, entityConfig.colMask);
 
 			}
 			random = 30;
 		}
-		reward0->addComponent<BoxCollider>(entityConfig.physicType, entityConfig.col, entityConfig.colMask);
+
 		reward0->addComponent<Reward>(random);
+
 
 	}
 }
